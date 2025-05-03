0c67c82df40efe053c3f021fab700e20ffe904e6
Vinoth <avinoth355@gmail.com>
2025-05-03 13:22:09 +0530

    added password field.

--- rest-demo/src/main/java/com/learning/rest_demo/controller/StudentController.java
+++ rest-demo/src/main/java/com/learning/rest_demo/controller/StudentController.java
@@ -1,46 +1,60 @@
 package com.learning.rest_demo.controller;
 
 import com.learning.rest_demo.model.Student;
+import com.learning.rest_demo.repository.StudentRepository;
 import com.learning.rest_demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/Student")
+@RequestMapping("/student")
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
 
+    @GetMapping("/login")
+    public String loginVerify(@RequestParam String username, @RequestParam String password){
+        Student stud = studentService.getStudent(username);
+
+        if((stud.getStudentId()!= null | stud.getStudentPassword() != null) | stud.getStudentId().equals(username) && stud.getStudentPassword().equals(password)){
+            return "Login Successfully";
+        }
+
+        return" Invalid Username / Password";
+    }
+
     // Read specific Student Details by their id from DB
-    @GetMapping("{studentId}")
+    @GetMapping("/get/{studentId}")
     public Student getStudent(@PathVariable("studentId") String studentId){
         return studentService.getStudent(studentId);
     }
 
     // Read all Student Details from DB
-    @GetMapping
+    @GetMapping("/getAll")
     public List<Student> getAllStudent(){
         return studentService.getAllStudent();
     }
 
     // Add Student Details in DB
-    @PostMapping
+    @PostMapping("/signup")
     public String createStudent(@RequestBody Student student){
         return studentService.createStudent(student);
     }
 
     // Update Student Details in DB
-    @PutMapping
+    @PutMapping("/update")
     public String updateStudent(@RequestBody Student student){
         return studentService.updateStudent(student);
     }
 
     // Delete Student by id in DB
-    @DeleteMapping("{studentId}")
+    @DeleteMapping("/delete/{studentId}")
     public String deleteStudent(@PathVariable("studentId") String studentId){
         return studentService.deleteStudent(studentId);
     }
+
 }
--- rest-demo/src/main/java/com/learning/rest_demo/model/Student.java
+++ rest-demo/src/main/java/com/learning/rest_demo/model/Student.java
@@ -9,10 +9,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
 @Table(name = "student_details")
 public class Student {
 
@@ -21,5 +21,56 @@ public class Student {
     private String studentName;
     private String studentClass;
     private String studentAddress;
+    private String studentPassword;
 
+    public Student(String studentId, String studentName, String studentClass, String studentAddress, String studentPassword) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studentClass = studentClass;
+        this.studentAddress = studentAddress;
+        this.studentPassword = studentPassword;
+    }
+
+    public Student() {
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentClass() {
+        return studentClass;
+    }
+
+    public void setStudentClass(String studentClass) {
+        this.studentClass = studentClass;
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    public String getStudentPassword() {
+        return studentPassword;
+    }
+
+    public void setStudentPassword(String studentPassword) {
+        this.studentPassword = studentPassword;
+    }
 }
